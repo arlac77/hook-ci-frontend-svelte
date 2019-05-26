830f484b62556647398224f24019a98fc85d905d
Markus Felten <Markus.Felten@gmx.de>
2019-05-27 01:32:44 +0200

    chore: initial checkin

---
+++ src/App.svelte
@@ -0,0 +1,14 @@
+<script>
+	export let name;
+	let src = 'tutorial/image.gif';
+</script>
+
+<style>
+	h1 {
+		color: purple;
+	}
+</style>
+
+<h1>Hello {name.toUpperCase()}!</h1>
+
+<img src={src} alt="A man dancing">
