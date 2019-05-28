cd80039473775980f2d5f69344668eb02ff0c8a4
Markus Felten <Markus.Felten@gmx.de>
2019-05-28 23:01:17 +0200

    fix: make use of svelte-way

--- src/App.svelte
+++ src/App.svelte
@@ -1,20 +1,41 @@
 <script>
-	import Queue from './pages/Queue.svelte';
+  import { Link, Switch, Route, Redirect } from "svelte-way";
+  import Queue from "./pages/Queue.svelte";
 
-	export let name;
+  export let name;
 </script>
 
 <style>
-	h1 {
-		color: purple;
-	}
+  h1 {
+    color: purple;
+  }
 </style>
 
+<ul>
+  <li>
+    <Link path="/">Index</Link>
+  </li>
+  <li>
+    <Link path="/queue">Qeueue</Link>
+  </li>
+</ul>
 
-
-<div><Queue></Queue></div>
-
-<a href="/queue" data-navigo>Qeueue</a>&nbsp;|&nbsp;
-
+<div>
+  <Queue />
+</div>
 
 <h1>Hello {name}!</h1>
+
+<Switch>
+  <Route exact path="/">
+    <h1>Home</h1>
+  </Route>
+
+  <Route exact path="/queue" let:params>
+    <h1>User with id: #{params.id} -> {params.section}</h1>
+  </Route>
+
+  <Route>
+    <h1>404</h1>
+  </Route>
+</Switch>
