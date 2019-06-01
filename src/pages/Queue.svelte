66758a7c082eb16153e5780375a12acdbb841d57
Markus Felten <Markus.Felten@gmx.de>
2019-06-01 09:25:43 +0200

    fix: use th

--- src/pages/Queue.svelte
+++ src/pages/Queue.svelte
@@ -1,9 +1,37 @@
 <script>
-  export let queue = { name: "" };
+  export let queue = { name: "", jobs: [] };
 </script>
+
 
 <div>
   <h1>Queue {queue.name}</h1>
    {queue.active} {queue.waiting} {queue.delayed} {queue.paused} {queue.completed}
   {queue.failed}
+  <table class="table">
+    <thead>
+      <tr>
+        <th>
+          <abbr title="Id">Id</abbr>
+        </th>
+        <th>
+          <abbr title="Started">S</abbr>
+        </th>
+      </tr>
+    </thead>
+    <tbody>
+      <!--
+    {#each queue.jobs as job (job.id)}
+      <tr>
+        <td>
+          <Link path="/queue/{queue.name}/job/{job.id}">{job.id}</Link>
+        </td>
+        <td>
+          {job.started}
+        </td>
+      </tr>
+    {/each}
+    -->
+    </tbody>
+  </table>
+
 </div>
--- src/pages/Queues.svelte
+++ src/pages/Queues.svelte
@@ -1,40 +1,35 @@
 <script>
   import { Link } from "svelte-way";
-  import { queues } from '../stores.mjs';
-
+  import { queues } from "../stores.mjs";
 </script>
 
 <div>
-  <table>
+  <table class="table is-bordered">
     <thead>
-      <tr><td>Name</td><td>Active</td><td>Waiting</td><td>Delayed</td><td>Paused</td><td>Completed</td><td>Failed</td></tr>
+      <tr>
+        <th><abbr title="Name">Name</abbr></th>
+        <th><abbr title="Active">A</abbr></th>
+        <th><abbr title="Waitng">W</abbr></th>
+        <th><abbr title="Delayed">D</abbr></th>
+        <th><abbr title="Paused">P</abbr></th>
+        <th><abbr title="Completed">C</abbr></th>
+        <th><abbr title="Failed">F</abbr></th>
+      </tr>
     </thead>
     <tbody>
-    {#each $queues as queue (queue.name)}
-      <tr>
-        <td>
-          <Link path="/queue/{queue.name}">{queue.name}</Link>
-        </td>
-        <td>
-          {queue.active}
-        </td>
-        <td>
-          {queue.waiting}
-        </td>
-        <td>
-          {queue.delayed}
-        </td>
-        <td>
-          {queue.paused}
-        </td>
-        <td>
-          {queue.completed}
-        </td>
-        <td>
-          {queue.failed}
-        </td>
-      </tr>
-    {/each}
+      {#each $queues as queue (queue.name)}
+        <tr>
+          <td>
+            <Link path="/queue/{queue.name}">{queue.name}</Link>
+          </td>
+          <td>{queue.active}</td>
+          <td>{queue.waiting}</td>
+          <td>{queue.delayed}</td>
+          <td>{queue.paused}</td>
+          <td>{queue.completed}</td>
+          <td>{queue.failed}</td>
+        </tr>
+      {/each}
     </tbody>
   </table>
 </div>
