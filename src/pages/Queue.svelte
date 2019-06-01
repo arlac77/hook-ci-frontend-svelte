e5844357b29327328a2c7dad0bd1c15bd84b02c3
Markus Felten <Markus.Felten@gmx.de>
2019-06-01 09:40:00 +0200

    style: reformat

--- src/pages/Queue.svelte
+++ src/pages/Queue.svelte
@@ -8,7 +8,6 @@
   $: queue = $queues.find(q => q.name === name);
 </script>
 
-
 <div>
   <h1>{queue.name}</h1>
    {queue.active} {queue.waiting} {queue.delayed} {queue.paused} {queue.completed}
@@ -25,18 +24,16 @@
       </tr>
     </thead>
     <tbody>
-      
-    {#each queue.jobs as job (job.id)}
-      <tr>
-        <td>
-          <Link path="/queue/{queue.name}/job/{job.id}">{job.id}</Link>
-        </td>
-        <td>
-          {job.started}
-        </td>
-      </tr>
-    {/each}
-    
+
+      {#each queue.jobs as job (job.id)}
+        <tr>
+          <td>
+            <Link path="/queue/{queue.name}/job/{job.id}">{job.id}</Link>
+          </td>
+          <td> {job.started} </td>
+        </tr>
+      {/each}
+
     </tbody>
   </table>
 
