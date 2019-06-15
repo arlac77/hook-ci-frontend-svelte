93f2a6a8cd2d7251c7c59f208c9a72ac95812ae2
Markus Felten <Markus.Felten@gmx.de>
2019-06-15 13:09:05 +0200

    fix: cleanup css

--- src/components/Navbar.svelte
+++ src/components/Navbar.svelte
@@ -7,17 +7,21 @@
   @import "../style/global.sass";
 </style>
 
-<nav class="header navbar is-fixed-top">
-  <div id="navbarExampleTransparentExample" class="navbar-menu">
+<nav class="navbar is-fixed-top">
+  <div class="navbar-menu">
     <div class="navbar-start">
       <div class="navbar-item has-dropdown is-hoverable">
-        <a on:click={() => nav.navigate('/home')}>Home</a>
+        <a href="/home" on:click={() => nav.navigate('/home')}>Home</a>
       </div>
       <div class="navbar-item has-dropdown is-hoverable">
-        <a class="anchored" on:click={() => nav.navigate('/queue/incoming')}>Incoming</a>
+        <a
+          href="/queue/incoming"
+          on:click={() => nav.navigate('/queue/incoming')}>
+          Incoming
+        </a>
       </div>
       <div class="navbar-item has-dropdown is-hoverable">
-        <a class="anchored" on:click={() => nav.navigate('/queues')}>Queues</a>
+        <a href="/queues" on:click={() => nav.navigate('/queues')}>Queues</a>
       </div>
     </div>
   </div>
