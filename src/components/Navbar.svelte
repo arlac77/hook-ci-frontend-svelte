2e7b6cc553128ca556bd628424b6bc0c1fa9daa6
Markus Felten <Markus.Felten@gmx.de>
2019-06-17 22:57:16 +0200

    fix: css wip

--- src/components/Navbar.svelte
+++ src/components/Navbar.svelte
@@ -4,24 +4,11 @@
 </script>
 
 <style>
+
 </style>
 
-<nav class="navbar is-fixed-top">
-  <div class="navbar-menu">
-    <div class="navbar-start">
-      <div class="navbar-item has-dropdown is-hoverable">
-        <a href="/home" on:click={() => nav.navigate('/home')}>Home</a>
-      </div>
-      <div class="navbar-item has-dropdown is-hoverable">
-        <a
-          href="/queue/incoming"
-          on:click={() => nav.navigate('/queue/incoming')}>
-          Incoming
-        </a>
-      </div>
-      <div class="navbar-item has-dropdown is-hoverable">
-        <a href="/queues" on:click={() => nav.navigate('/queues')}>Queues</a>
-      </div>
-    </div>
-  </div>
+<nav>
+  <a href="/home" on:click={() => nav.navigate('/home')}>Home</a>
+  <a href="/queue/incoming" on:click={() => nav.navigate('/queue/incoming')}>Incoming</a>
+  <a href="/queues" on:click={() => nav.navigate('/queues')}>Queues</a>
 </nav>
