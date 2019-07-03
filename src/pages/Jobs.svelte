bcbe064052240a47e089c692936abe3b6a163fd1
Markus Felten <Markus.Felten@gmx.de>
2019-07-03 16:36:20 +0200

    wip

---
+++ src/pages/Jobs.svelte
@@ -0,0 +1,34 @@
+<script>
+  import RouterLink from "svelte-easyroute-rollup/RouterLink.svelte";
+  import { queues } from "../stores.mjs";
+  export let currentRoute;
+  export let queue = { name: "", jobs: [] };
+
+  $: queue = $queues.find(q => q.name === currentRoute.params.name);
+</script>
+
+<div>
+  <table class="table is-bordered is-striped is-hoverable">
+    <thead>
+      <tr>
+        <th>Id</th>
+        <th>Processed On</th>
+        <th>Repository</th>
+        <th>Branch</th>
+      </tr>
+    </thead>
+    <tbody>
+      {#each queue.jobs as job (job.id)}
+        <tr>
+          <td>
+            <RouterLink to="/queue/{currentRoute.params.queue}/job/{job.id}" text={job.id} />
+          </td>
+          <td>{job.id}</td>
+          <td>{job.processedOn}</td>
+          <td>{job.repository}</td>
+          <td>{job.branch}</td>
+        </tr>
+      {/each}
+    </tbody>
+  </table>
+</div>
