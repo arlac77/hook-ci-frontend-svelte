521559d16c336d0251f423bfa9fcf320bade174b
Markus Felten <Markus.Felten@gmx.de>
2019-07-04 09:32:05 +0200

    chore(rollup): consult base from package.json

--- rollup.config.js
+++ rollup.config.js
@@ -10,11 +10,11 @@ import history from "connect-history-api-fallback";
 import proxy from "http-proxy-middleware";
 import express from "express";
 import { create as browserSyncFactory } from "browser-sync";
-import pkg from './package.json';
+import { base } from './package.json';
 
 const production = !process.env.ROLLUP_WATCH;
 const api = "/api";
-const proxyTarget = "https://mfelten.dynv6.net/services/ci/";
+const proxyTarget = `https://mfelten.dynv6.net${base}`;
 const dist = "public";
 
 export default {
--- src/pages/About.svelte
+++ src/pages/About.svelte
@@ -1,9 +1,22 @@
 <script>
-  import { name, version, description, base } from "../../package.json";
+  import { name, version, description, base, api } from "../../package.json";
 </script>
 
 <div>
   <h3>{name}</h3>
   <p>{description}</p>
-  Version: {version} Mounted: {base}
+  <table class="table is-bordered is-striped is-hoverable">
+    <tr>
+      <td>Version</td>
+      <td>{version}</td>
+    </tr>
+    <tr>
+      <td>Mounted</td>
+      <td>{base}</td>
+    </tr>
+    <tr>
+      <td>API</td>
+      <td>{api}</td>
+    </tr>
+  </table>
 </div>
