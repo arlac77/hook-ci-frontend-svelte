26c01a8697903d47f1cfc57bcbb52f69eb094099
Markus Felten <Markus.Felten@gmx.de>
2019-08-10 23:54:59 +0200

    fix: format date

--- src/components/JobTable.svelte
+++ src/components/JobTable.svelte
@@ -1,7 +1,14 @@
 <script>
   import { Link } from "svelte-guard-history-router";
   export let jobs = [];
-  export let queue= { name: 'xxx' };
+  export let queue = { name: "xxx" };
+
+  const formatter = new Intl.DateTimeFormat("en", {
+    hour12: false,
+    hour: "numeric",
+    minute: "2-digit",
+    second: "2-digit"
+  });
 </script>
 
 <div>
@@ -20,8 +27,12 @@
           <td>
             <Link href="/queue/{queue.name}/job/{job.id}">{job.id}</Link>
           </td>
-          <td>{job.processedOn}</td>
-          <td><Link href="/repository/{job.repository.name}">{job.repository.name}</Link></td>
+          <td>{formatter.format(new Date(job.processedOn))}</td>
+          <td>
+            <Link href="/repository/{job.repository.name}">
+              {job.repository.name}
+            </Link>
+          </td>
           <td>{job.branch}</td>
         </tr>
       {/each}
