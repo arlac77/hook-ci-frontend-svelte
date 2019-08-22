b2d6ad832424caa32781b3fa41eb17c99da5e24d
Markus Felten <Markus.Felten@gmx.de>
2019-08-22 15:43:05 +0200

    fix(css): wip

--- src/components/App.svelte
+++ src/components/App.svelte
@@ -5,16 +5,29 @@
 
 <div class="wrapper">
   <header id="topnav">
-    <Link href="/">Home</Link>
-    <Link href="/queue">Queues</Link>
-    <Link href="/repository">Repositories</Link>
-    <Link href="/node">Nodes</Link>
-    <Link href="/about">About</Link>
-    <Link href="/login">Login</Link>
+    <Link href="/">
+      <img class="logo" src="hook_ci.svg" alt="Hook CI" />
+      <h2>Hook CI</h2>
+    </Link>
+    <ul>
+      <li>
+        <Link href="/queue">Queues</Link>
+      </li>
+      <li>
+        <Link href="/repository">Repositories</Link>
+      </li>
+      <li>
+        <Link href="/node">Nodes</Link>
+      </li>
+      <li>
+        <Link href="/about">About</Link>
+      </li>
+      <li>
+        <Link href="/login">Login</Link>
+      </li>
+    </ul>
   </header>
   <main>
-    <Outlet {router}>
-      nothing there
-    </Outlet>
+    <Outlet {router}>nothing there</Outlet>
   </main>
 </div>
