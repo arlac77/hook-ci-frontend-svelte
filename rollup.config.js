1d454bd87b324a4207b841fafcb0aa410c1d6746
Markus Felten <Markus.Felten@gmx.de>
2019-09-05 12:22:30 +0200

    chore(rollup): start light-server from rollup

--- rollup.config.js
+++ rollup.config.js
@@ -3,10 +3,31 @@ import resolve from "rollup-plugin-node-resolve";
 import commonjs from "rollup-plugin-commonjs";
 import json from "rollup-plugin-json";
 import { terser } from "rollup-plugin-terser";
-import copy from 'rollup-plugin-copy';
+import copy from "rollup-plugin-copy";
+import { spawn } from "child_process";
+import { config } from "./package.json";
 
 const production = !process.env.ROLLUP_WATCH;
 const dist = "public";
+
+if (!production) {
+  const ls = spawn("./node_modules/.bin/light-server", [
+    "-s",
+    dist,
+    "--servePrefix",
+    config.urlPrefix,
+    "--historyindex",
+    config.urlPrefix + "/index.html",
+    "-p",
+    "5000",
+    "--proxypath",
+    config.api,
+    "-x",
+    config.proxyTarget,
+    "-w", `${dist}/*.mjs,${dist}/*.css`
+  ]);
+  ls.stdout.pipe(process.stdout);
+}
 
 export default {
   input: "src/main.mjs",
@@ -17,9 +38,7 @@ export default {
   },
   plugins: [
     copy({
-      targets: [
-        { src: 'node_modules/mf-styling/global.css', dest: dist }
-      ]
+      targets: [{ src: "node_modules/mf-styling/global.css", dest: dist }]
     }),
     svelte({
       dev: !production
