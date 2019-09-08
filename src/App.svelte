46b810c655bbececf0f7a4d5dc86bc7ac4229a78
Markus Felten <Markus.Felten@gmx.de>
2019-09-09 00:04:54 +0200

    fix(style): get rid of .wrapper

--- src/App.svelte
+++ src/App.svelte
@@ -8,51 +8,59 @@
   }
 </script>
 
-<div class="wrapper">
-  <nav>
-    <Link href="/">
-      <img class="logo" src="hook_ci.svg" alt="Hook CI" />
-      <h2>Hook CI</h2>
-    </Link>
-    <ul>
-      <li>
-        <Link href="/queue"><h3>Queues</h3></Link>
-      </li>
-      <li>
-        <Link href="/repository"><h3>Repositories</h3></Link>
-      </li>
-      <li>
-        <Link href="/node"><h3>Nodes</h3></Link>
-      </li>
-      <li>
-        <Link href="/about"><h3>About</h3></Link>
-      </li>
-      <li>
-        {#if $session.isValid}
-          <Menue>
-            <h3 slot="title" class="dropdown-trigger">{$session.username}</h3>
-            <ul class="dropdown-content">
-              <li>
-                <a href="/" on:click|preventDefault={logout}>
-                  Logout {$session.username}
-                </a>
-              </li>
-              <li>
-                <a href="#!">two</a>
-              </li>
-              <li class="divider" />
-              <li>
-                <a href="#!">three</a>
-              </li>
-            </ul>
-          </Menue>
-        {:else}
-          <Link href="/login"><h3>Login</h3></Link>
-        {/if}
-      </li>
-    </ul>
-  </nav>
-  <main>
-    <Outlet {router}>nothing there</Outlet>
-  </main>
-</div>
+<nav>
+  <Link href="/">
+    <img class="logo" src="hook_ci.svg" alt="Hook CI" />
+    <h2>Hook CI</h2>
+  </Link>
+  <ul>
+    <li>
+      <Link href="/queue">
+        <h3>Queues</h3>
+      </Link>
+    </li>
+    <li>
+      <Link href="/repository">
+        <h3>Repositories</h3>
+      </Link>
+    </li>
+    <li>
+      <Link href="/node">
+        <h3>Nodes</h3>
+      </Link>
+    </li>
+    <li>
+      <Link href="/about">
+        <h3>About</h3>
+      </Link>
+    </li>
+    <li>
+      {#if $session.isValid}
+        <Menue>
+          <h3 slot="title" class="dropdown-trigger">{$session.username}</h3>
+          <ul class="dropdown-content">
+            <li>
+              <a href="/" on:click|preventDefault={logout}>
+                Logout {$session.username}
+              </a>
+            </li>
+            <li>
+              <a href="#!">two</a>
+            </li>
+            <li class="divider" />
+            <li>
+              <a href="#!">three</a>
+            </li>
+          </ul>
+        </Menue>
+      {:else}
+        <Link href="/login">
+          <h3>Login</h3>
+        </Link>
+      {/if}
+    </li>
+  </ul>
+</nav>
+<main>
+  <Outlet {router}>nothing there</Outlet>
+</main>
