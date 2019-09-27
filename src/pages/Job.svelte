114a7bef93e5d139431d470dcdebd5645ed15742
Markus Felten <Markus.Felten@gmx.de>
2019-09-27 22:35:03 +0200

    feat: job rerun & cancel

--- src/pages/Job.svelte
+++ src/pages/Job.svelte
@@ -1,28 +1,45 @@
 <script>
   import { onDestroy } from "svelte";
   import { Link } from "svelte-guard-history-router";
+  import { ActionButton } from "svelte-common";
   import Step from "../components/Step.svelte";
-  import { queue, job } from "../main.mjs";
+  import { queue, session, job } from "../main.mjs";
+  import { config } from "../../package.json";
 
   export let state;
+
+  async function rerun() {
+    return fetch(`${config.api}/queue/${$queue.name}/job/${$job.id}/rerun`, {
+      method: "POST",
+      headers: session.authorizationHeader
+    });
+  }
+
+  async function cancel() {
+    return fetch(`${config.api}/queue/${$queue.name}/job/${$job.id}/cancel`, {
+      method: "POST",
+      headers: session.authorizationHeader
+    });
+  }
 </script>
 
 {#if $job}
   <div>
     <h3>Job {$job.id}</h3>
     AttemptsMade: {$job.attemptsMade} Node: {$job.node}
+    <ActionButton action={rerun}>Rerun</ActionButton>
+    <ActionButton action={cancel}>Cancel</ActionButton>
     <Link href="/queue/{$queue.name}/job/{$job.id}/log">Log</Link>
 
-    <!--
-  {#if $job}
-  <ul>
-    {#each $job.steps as step, i}
-      <li>
-        <Step {step} />
-      </li>
-    {/each}
-  </ul>
-  {/if}
-  -->
+    {#if $job}
+      <ul>
+        {#each $job.steps as step, i}
+          <li>
+            <Step {step} />
+          </li>
+        {/each}
+      </ul>
+    {:else}no jobs{/if}
+
   </div>
 {/if}
