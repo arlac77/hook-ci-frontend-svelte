02acdcabde0194489a27d68813ac3980bc3cbae7
Markus Felten <Markus.Felten@gmx.de>
2019-10-03 17:08:56 +0200

    fix: usa absolut logo url

--- src/App.svelte
+++ src/App.svelte
@@ -2,6 +2,7 @@
   import { Outlet, link, active } from "svelte-guard-history-router";
   import { Menue } from "svelte-common";
   import { router, session } from "./main.mjs";
+  import { config } from "../package.json";
 
   function logout() {
     session.invalidate();
@@ -10,7 +11,7 @@
 
 <nav>
   <a href="/" use:link={router} use:active={router}>
-    <img class="logo" src="hook_ci.svg" alt="Hook CI" />
+    <img class="logo" src="{config.urlPrefix}/hook_ci.svg" alt="Hook CI" />
     Hook CI
   </a>
   <ul class="left">
