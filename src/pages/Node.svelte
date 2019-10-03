2c87687802d8ee17b1efa7fec029e076bc54e913
Markus Felten <Markus.Felten@gmx.de>
2019-10-03 02:57:32 +0200

    feat: stop / restart nodes

--- src/pages/Node.svelte
+++ src/pages/Node.svelte
@@ -1,11 +1,21 @@
 <script>
   import { formatDuration, ActionButton } from "svelte-common";
-  import { node } from "../main.mjs";
+  import { node, session } from "../main.mjs";
+  import { config } from "../../package.json";
 
   async function restart() {
-      
+    return fetch(`${config.api}/node/${$node.name}/restart`, {
+      method: "POST",
+      headers: session.authorizationHeader
+    });
   }
 
+  async function stop() {
+    return fetch(`${config.api}/node/${$node.name}/stop`, {
+      method: "POST",
+      headers: session.authorizationHeader
+    });
+  }
 </script>
 
 {#if $node}
@@ -16,4 +26,5 @@
   {/if}
 
   <ActionButton action={restart}>Restart</ActionButton>
+  <ActionButton action={stop}>Stop</ActionButton>
 {:else}Not found{/if}
