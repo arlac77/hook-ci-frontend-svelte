32b19f32d80aaf75fb4f28e246673e600bdbd2c5
Markus Felten <Markus.Felten@gmx.de>
2019-10-15 15:40:03 +0200

    fix: add unknown hint if step has not yet been processed

---
+++ src/components/Requirements.svelte
@@ -0,0 +1,18 @@
+<script>
+  export let requirements;
+</script>
+
+<table>
+  <thead>
+    <th aria-sort="none">Executable</th>
+    <th>Version</th>
+  </thead>
+  <tbody>
+    {#each requirements as r}
+      <tr>
+        <td>{r.executable}</td>
+        <td>{r.version}</td>
+      </tr>
+    {/each}
+  </tbody>
+</table>
--- src/components/Step.svelte
+++ src/components/Step.svelte
@@ -1,33 +1,29 @@
 <script>
   import { formatSecondsSinceEpoch } from "svelte-common";
   import NodeLink from "./NodeLink.svelte";
+  import Requirements from "./Requirements.svelte";
   export let step;
 </script>
 
 {#if step}
-<div>
-  <h3>{step.name}</h3>
-  {step.executable}
-  {#each step.args as a}{a}{/each}
-  Started: {formatSecondsSinceEpoch(step.started)}
-  Ended: {formatSecondsSinceEpoch(step.ended)}
-  <NodeLink node={step.node}/>
-
-  {#if step.requirements}
-  <table>
-    <thead>
-      <th aria-sort="none">Executable</th>
-      <th>Version</th>
-    </thead>
-    <tbody>
-      {#each step.requirements as r}
-        <tr>
-          <td>{r.executable}</td>
-          <td>{r.version}</td>
-        </tr>
-      {/each}
-    </tbody>
-  </table>
-  {/if}
-</div>
+  <div>
+    <h3>{step.name}</h3>
+    {step.executable}&#160;
+    {#each step.args as a}{a}{/each}
+    started: {formatSecondsSinceEpoch(step.started)}
+    {#if step.ended}
+      ended: {formatSecondsSinceEpoch(step.ended)} exit: {step.exitCode}
+      {#if step.ok}
+        <abbr class="ok-hint" />
+      {:else}
+        <abbr class="error-hint" />
+      {/if}
+    {:else}
+      <abbr class="unknown-hint" />
+    {/if}
+    <NodeLink node={step.node} />
+    {#if step.requirements}
+      <Requirements requirements={step.requirements} />
+    {/if}
+  </div>
 {/if}
