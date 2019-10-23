4ff45a1a95f6d9142920f151bf2a650be495b97c
Markus Felten <Markus.Felten@gmx.de>
2019-10-24 01:11:25 +0200

    feat: prev/next job

--- src/pages/Job.svelte
+++ src/pages/Job.svelte
@@ -4,29 +4,48 @@
   import { ActionButton } from "svelte-common";
   import NodeLink from "../components/NodeLink.svelte";
   import Step from "../components/Step.svelte";
-  import { queue, session, job } from "../main.mjs";
+  import { queue, session, job, router } from "../main.mjs";
   import { config } from "../../package.json";
 
   export let state;
 
   async function jobAction(suffix) {
-    const response = await fetch(`${config.api}/queue/${$queue.name}/job/${$job.id}/${suffix}`, {
-      method: "POST",
-      headers: session.authorizationHeader
-    });
+    const response = await fetch(
+      `${config.api}/queue/${$queue.name}/job/${$job.id}/${suffix}`,
+      {
+        method: "POST",
+        headers: session.authorizationHeader
+      }
+    );
 
-    if(!response.ok) {
+    if (!response.ok) {
       throw new Error(response.statusText);
+    }
+  }
+
+  async function next() {
+    const id = parseInt(router.keys.job.value) + 1;
+    router.push(`/queue/${$queue.name}/job/${id}`);
+  }
+
+  async function previous() {
+    const id = parseInt(router.keys.job.value) - 1;
+    if (id > 0) {
+      router.push(`/queue/${$queue.name}/job/${id}`);
     }
   }
 </script>
 
+<ActionButton action={next}>Next</ActionButton>
+<ActionButton action={previous}>Previous</ActionButton>
+
 {#if $job}
   <div>
     <h3>Job {$job.id}</h3>
-    AttemptsMade: {$job.attemptsMade} <NodeLink node={$job.node}/>
-    <ActionButton action={()=>jobAction('rerun')}>Rerun</ActionButton>
-    <ActionButton action={()=>jobAction('cancel')}>Cancel</ActionButton>
+    AttemptsMade: {$job.attemptsMade}
+    <NodeLink node={$job.node} />
+    <ActionButton action={() => jobAction('rerun')}>Rerun</ActionButton>
+    <ActionButton action={() => jobAction('cancel')}>Cancel</ActionButton>
     <Link href="/queue/{$queue.name}/job/{$job.id}/log">Log</Link>
 
     {#if $job && $job.steps}
