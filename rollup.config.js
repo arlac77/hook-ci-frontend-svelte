8e865e32f5c295fa1d3f61f877c02320edaea075
Markus Felten <Markus.Felten@gmx.de>
2019-12-01 14:44:32 +0100

    chore(package): remove @rollup/plugin-json@^4.0.0 chore(package): add rollup-plugin-consts@^1.0.1 chore(rollup): import consts chore(rollup): add consts

--- rollup.config.js
+++ rollup.config.js
@@ -1,3 +1,4 @@
+import consts from 'rollup-plugin-consts';
 import svelte from "rollup-plugin-svelte";
 import resolve from "rollup-plugin-node-resolve";
 import commonjs from "rollup-plugin-commonjs";
