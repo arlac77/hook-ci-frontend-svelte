46e7a1650493f4c6117b9b53bf28c8cd7bb2e5a1
Markus Felten <Markus.Felten@gmx.de>
2019-12-10 20:09:01 +0100

    fix: add acornClassFields

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,5 @@
 import acornClassFields from "acorn-class-fields";
-import consts from 'rollup-plugin-consts';
+import consts from "rollup-plugin-consts";
 import svelte from "rollup-plugin-svelte";
 import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "rollup-plugin-commonjs";
@@ -19,27 +19,34 @@ export default {
     format: "esm",
     file: `${dist}/bundle.mjs`
   },
-  plugins: [consts({
-    name,
-    version,
-    description,
-    ...config
-  }), copy({
-    targets: [
-      { src: 'node_modules/mf-styling/global.css', dest: dist }
-    ]
-  }), svelte({
-    dev: !production,
-    css: css => {
-      css.write(`${dist}/bundle.css`);
-    }
-  }), resolve({ browser: true }), commonjs(), production && terser(), dev({
-    port,
-    dirs: [dist],
-    spa: `${dist}/index.html`,
-    basePath: config.base,
-    proxy: { [`${config.api}/*`]: [config.proxyTarget, { https: true }] }
-  })],
+  plugins: [
+    consts({
+      name,
+      version,
+      description,
+      ...config
+    }),
+    copy({
+      targets: [{ src: "node_modules/mf-styling/global.css", dest: dist }]
+    }),
+    svelte({
+      dev: !production,
+      css: css => {
+        css.write(`${dist}/bundle.css`);
+      }
+    }),
+    resolve({ browser: true }),
+    commonjs(),
+  //  production && terser(),
+    dev({
+      port,
+      dirs: [dist],
+      spa: `${dist}/index.html`,
+      basePath: config.base,
+      proxy: { [`${config.api}/*`]: [config.proxyTarget, { https: true }] }
+    })
+  ],
+  acornInjectPlugins: [acornClassFields],
   watch: {
     clearScreen: false
   }
