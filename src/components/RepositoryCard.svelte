d649e6d531ef5f603ae61c4778292d585ec40c5e
Markus Felten <Markus.Felten@gmx.de>
2020-04-11 12:41:24 +0200

    fix: add Archived title

--- src/components/RepositoryCard.svelte
+++ src/components/RepositoryCard.svelte
@@ -7,8 +7,8 @@
   <div class="card-content">
     <h5 class="card-title">{repository.name}</h5>
     <h6 class="card-subtitle">{repository.description}</h6>
-    <p class="card-text">{repository.id}</p>
-    <p class="card-text">{repository.isArchived}</p>
+    <p class="card-text">Id {repository.id}</p>
+    <p class="card-text">Archived {repository.isArchived}</p>
   </div>
   <ul>
     {#each repository.urls as url}
