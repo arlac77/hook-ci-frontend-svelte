8ace55a21bc99b1d3fdfa995deda6dda0c9b60e4
Markus Felten <Markus.Felten@gmx.de>
2020-05-02 01:43:47 +0200

    fix: add repo group url

--- src/components/RepositoryGroupCard.svelte
+++ src/components/RepositoryGroupCard.svelte
@@ -7,5 +7,6 @@
     <div class="card-title">{repositoryGroup.name}</div>
     {#if repositoryGroup.description}{repositoryGroup.description}{/if}
     {#if repositoryGroup.id}{repositoryGroup.id}{/if}
+    <a href={repositoryGroup.url}>Home</a>
   </div>
 </div>
