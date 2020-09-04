23cd5dfbc928030951e64a46c6c08ef6aa7f3b89
Markus Felten <Markus.Felten@gmx.de>
2020-09-04 16:47:09 +0200

    fix: use SessionDetail & About from common

--- src/pages/About.svelte
+++ src/pages/About.svelte
@@ -1,55 +1,18 @@
 <script>
-  import { DateTime } from "svelte-common";
+  import { DateTime, About, SessionDetails } from "svelte-common";
   import { session } from "../main.mjs";
 
-  import name from 'consts:name';
-  import version from 'consts:version';
-  import description from 'consts:description';
-  import base from 'consts:base';
-  import api from 'consts:api';
-  import graphQl from 'consts:graphQl';
-
+  import name from "consts:name";
+  import version from "consts:version";
+  import description from "consts:description";
+  import graphQl from "consts:graphQl";
 </script>
 
-<div>
-  <h2>{name}</h2>
-  <p>{description}</p>
-  <table class="bordered striped hoverable">
-    <tbody>
-      <tr>
-        <td>Version</td>
-        <td>{version}</td>
-      </tr>
-      <tr>
-        <td>Mounted</td>
-        <td>{base}</td>
-      </tr>
-      <tr>
-        <td>API</td>
-        <td>{api}</td>
-      </tr>
-      <tr>
-        <td>Graphql API</td>
-        <td>{graphQl}</td>
-      </tr>
-      <tr>
-        <td>Username</td>
-        <td>{$session.username}</td>
-      </tr>
-      <tr>
-        <td>Session Expiration</td>
-        <td class={$session.isValid ? 'ok' : 'error'}>
-          <DateTime date={$session.expirationDate}/>
-        </td>
-      </tr>
-      <tr>
-        <td>Entitlements</td>
-        <td>
-          {#each [...$session.entitlements].sort() as name}
-            <div>{name}</div>
-          {/each}
-        </td>
-      </tr>
-    </tbody>
-  </table>
-</div>
+<About {name} {version} {description}>
+  <tr />
+  <tr>
+    <td>Graphql API</td>
+    <td>{graphQl}</td>
+  </tr>
+  <SessionDetails session={$session} />
+</About>
